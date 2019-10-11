b9e12be5cd6e2807ff646c8859f386aaba1a870c
yanlin <362460453@qq.com>
2019-10-11 17:07:52 +0800

    file

---
+++ file-demo/src/main/java/com/file/FileApplication.java
@@ -0,0 +1,20 @@
+package com.file;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * @ClassName MmcFileApplication
+ * @Description: TODO
+ * @Author yanlin
+ * @Date 2019/10/9
+ * @Version V1.0
+ **/
+@SpringBootApplication
+public class FileApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FileApplication.class, args);
+    }
+
+}
